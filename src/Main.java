5e4056ec83ace6137ad533a22d4f323071458123
IRINA <aksenemko@icloud.com>
2024-11-21 13:15:59 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 100;
-        int y = 1100;
+        int initialBalance = 1300;
+        int addend = 1100;
 
-        int balance = x + y;
-
-        int bonus;
-        if (y > 1000) {
-            bonus = y / 100;
-        } else {
-            bonus = 0;
+        int bonus = 0;
+        if (addend > 1000) {
+            bonus = addend / 100;
         }
 
-        System.out.println("Ваш баланс:");
-        System.out.print(balance + bonus);
+        int finalBonus = initialBalance + addend + bonus;
+        System.out.println("Ваш баланс: " + finalBonus);
+        System.out.print("Ваш бонус: " + bonus);
 
     }
 }
