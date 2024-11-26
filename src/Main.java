c505f759638a2a227085cc84419b45066d77bdec
IRINA <aksenemko@icloud.com>
2024-11-26 14:01:24 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ public class Main {
 
         int initialBalance = 1300;
         int addend = 1100;
-
         int bonus = 0;
+
         if (addend > 1000) {
             bonus = addend / 100;
         }
 
         int finalBonus = initialBalance + addend + bonus;
+
         System.out.println("Ваш баланс: " + finalBonus);
         System.out.print("Ваш бонус: " + bonus);
-
     }
 }
